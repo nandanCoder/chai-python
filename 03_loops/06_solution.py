a06f00635605e57765c76c58845c5c150ae0441b
CoderNandan <nandanindia1947@gmail.com>
2024-02-18 21:22:19 +0530

    loops

---
+++ 03_loops/06_solution.py
@@ -0,0 +1,8 @@
+
+input_str = "teeteracdacd"
+
+for char in input_str:
+  if  input_str.count(char) == 1:
+    print("Result is",char)
+    # loop katam kar databhmm
+    break
---
+++ 03_loops/08_solution.py
@@ -0,0 +1,7 @@
+while True:
+    number = int(input("Enter a value 1 to 10", ))
+    if 1<= number <=10:
+        print("Thanks")
+        break
+    else:
+        print("Please enter a number between 1 and 10")
